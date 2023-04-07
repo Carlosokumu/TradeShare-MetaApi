d160078faaf643bb27c24569a2e46d544cdf64e0
Carlos <carlosokumu254@gmail.com>
2023-04-07 20:17:36 +0300

    json parse

--- server.js
+++ server.js
@@ -151,6 +151,10 @@ app.get("/history",async (req,res) => {
 
 
 app.post("/mt4info",(req,res) => {
+   var jsonObj = JSON.parse(req.body);
+   for(var myKey in jsonObj) {
+      console.log("key:"+myKey+", value:"+myJson[myKey]);
+   }
    console.log("MT4 Posting data....")
    console.log(req.body)
    res.status(200).send("Link")
