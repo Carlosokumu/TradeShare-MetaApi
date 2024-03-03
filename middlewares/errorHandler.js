29c49485f96ade47b2c675a7c570b5732bf8f377
Carlos <carlosokumu254@gmail.com>
2024-03-03 23:35:12 +0300

    add an error handler middleware

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,13 @@
+const errorHandler = (err, req, res, next) => {
+  console.error(err); 
+  // Create a copy of the error object
+  let error = { ...err };
+  error.message = err.message;
+
+  res.status(error.statusCode || 500).json({
+    success: false,
+    data: { error: error.message || "Internal Server Error" },
+  });
+};
+
+module.exports = errorHandler;
