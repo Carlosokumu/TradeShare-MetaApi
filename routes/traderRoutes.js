71bea66e4826cb2c930b9f248b833f546ead3301
Carlos <carlosokumu254@gmail.com>
2024-03-03 23:34:40 +0300

    implement tradeshare registeration route for traders

---
+++ routes/traderRoutes.js
@@ -0,0 +1,13 @@
+
+const express = require("express");
+const router = express.Router();
+const registerTrader = require('../controllers/traderController')
+
+
+
+
+router.post('/register',registerTrader)
+
+
+
+module.exports = router;
