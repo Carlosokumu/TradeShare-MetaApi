f6258a8d8200cf719b7beb17b1aec00bc3ed9b74
Carlos <carlosokumu254@gmail.com>
2024-03-06 09:26:06 +0300

    register api endpoints to router

--- routes/tradeshareRoutes.js
+++ routes/tradeshareRoutes.js
@@ -1,12 +1,16 @@
-
 const express = require("express");
 const router = express.Router();
 
-
-
 const traderRoutes = require("./traderRoutes");
-
+const {
+  getAccountEquityChart,
+  getAccountOpenPositions,
+  getAccountHistoricalTrades,
+} = require("../controllers/tradeshareController.js");
 
 router.use("/v1/trader", traderRoutes);
+router.use("/v1/equity_chart", getAccountEquityChart);
+router.use("/v1/positions", getAccountOpenPositions);
+router.use("/v1/historicaltrades", getAccountHistoricalTrades);
 
 module.exports = router;
