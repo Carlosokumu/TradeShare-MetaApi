81a0abcf5c63ce804a978d8119b4f077c89af904
Carlos <carlosokumu254@gmail.com>
2024-03-06 09:25:38 +0300

    implement tradeshare specific routes

---
+++ controllers/tradeshareController.js
@@ -0,0 +1,142 @@
+let MetaApi = require("metaapi.cloud-sdk").default;
+const config = require("../config");
+const token = process.env.ACCOUNT_TOKEN || config.accessToken;
+const ErrorResponse = require("../utils/ErrorResponse");
+let MetaStats = require("metaapi.cloud-sdk").MetaStats;
+const asyncHandler = require("../middlewares/asyncHandler");
+const getTimeDifference = require("../utils/TimeUtil");
+
+const metaStats = new MetaStats(token);
+const api = new MetaApi(token);
+
+const TODAY = 1;
+const WEEK = 2;
+const MONTH = 3;
+const DEFAULT_OFFSET = 0;
+const DEFAULT_LIMIT = 20;
+
+const getAccountEquityChart = asyncHandler(async (req, res, next) => {
+  const accountId = req.query.account_id;
+  if (!accountId) {
+    return next(new ErrorResponse("Please provide an account id", 400));
+  }
+  try {
+    const metrics = await metaStats.getMetrics(accountId);
+    return res.status(200).json({
+      metrics: metrics,
+    });
+  } catch (error) {
+    return next(new ErrorResponse(error, 500));
+  }
+});
+
+const getAccountOpenPositions = asyncHandler(async (req, res, next) => {
+  const accountId = req.query.account_id;
+  if (!accountId) {
+    return next(new ErrorResponse("Please provide an account id", 400));
+  }
+  try {
+    const account = await api.metatraderAccountApi.getAccount(accountId);
+    const connection = account.getRPCConnection();
+    await connection.connect();
+    await connection.waitSynchronized();
+    const positions = await connection.getPositions();
+    return res.status(200).json({
+      positions: positions,
+    });
+  } catch (error) {
+    return next(new ErrorResponse(error, 500));
+  }
+});
+
+const getAccountHistoricalTrades = asyncHandler(async (req, res, next) => {
+  const { account_id, history_range, offset } = req.query;
+
+  const trades = [];
+
+  if (!account_id || !history_range) {
+    return next(
+      new ErrorResponse(
+        "account_id or history range parameter are required",
+        400
+      )
+    );
+  }
+
+  const currentDate = new Date();
+  let startDate;
+
+  switch (parseInt(history_range)) {
+    case TODAY:
+      startTimeToday = new Date(currentDate);
+      startTimeToday.setHours(0, 0, 0, 0);
+      startTimeToday.setHours(startTimeToday.getHours() + 3);
+      startDate = startTimeToday;
+      break;
+    case WEEK:
+      startForWeek = new Date(currentDate);
+      startForWeek.setDate(startForWeek.getDate() - 7);
+      startForWeek.setHours(0, 0, 0, 0);
+      startForWeek.setHours(startForWeek.getHours() + 3);
+      startDate = startForWeek;
+      break;
+    case MONTH:
+      const oneMonthAgo = new Date(currentDate);
+      oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
+      oneMonthAgo.setHours(0, 0, 0, 0);
+      oneMonthAgo.setHours(oneMonthAgo.getHours() + 3);
+      startDate = oneMonthAgo;
+      break;
+  }
+
+  try {
+    const account = await api.metatraderAccountApi.getAccount(account_id);
+    const connection = account.getRPCConnection();
+    await connection.connect();
+    await connection.waitSynchronized();
+
+    const orders = await connection.getHistoryOrdersByTimeRange(
+      startDate,
+      currentDate,
+      parseInt(offset) || DEFAULT_OFFSET,
+      DEFAULT_LIMIT
+    );
+    for (var i = 0; i < orders.historyOrders.length; i++) {
+      const position = await connection.getDealsByPosition(
+        orders.historyOrders[i].positionId
+      );
+      const closedPositions = position.deals.filter(
+        (deal) => deal.entryType === "DEAL_ENTRY_OUT"
+      );
+      for (var j = 0; j < closedPositions.length; j++) {
+        if (!trades.map((trade) => trade.id).includes(closedPositions[j].id)) {
+          trades.push({
+            id: closedPositions[j].id,
+            type: closedPositions[j].type,
+            profit: closedPositions[j].profit,
+            symbol: closedPositions[j].symbol,
+            createdAt: getTimeDifference(closedPositions[j].time),
+            volume: closedPositions[j].volume,
+            time: closedPositions[j].time,
+          });
+        }
+      }
+    }
+    res.status(200).json({
+      trades: trades.sort((a, b) => Date.parse(b.time) - Date.parse(a.time)),
+    });
+  } catch (error) {
+    console.log("Error", error);
+    if (error.message) {
+      return next(new ErrorResponse(error.message, error.status));
+    } else {
+      return next(new ErrorResponse(error.message, error));
+    }
+  }
+});
+
+module.exports = {
+  getAccountEquityChart,
+  getAccountOpenPositions,
+  getAccountHistoricalTrades,
+};
