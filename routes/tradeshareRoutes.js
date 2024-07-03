cda3fa12f9619996478b4f5b4a7dab62aa17dab2
Carlos <carlosokumu254@gmail.com>
2024-07-03 09:56:41 +0300

    register enpoint for searching a server name

--- routes/tradeshareRoutes.js
+++ routes/tradeshareRoutes.js
@@ -6,11 +6,13 @@ const {
   getAccountEquityChart,
   getAccountOpenPositions,
   getAccountHistoricalTrades,
+  searchServerName,
 } = require("../controllers/tradeshareController.js");
 
 router.use("/v1/trader", traderRoutes);
 router.use("/v1/equity_chart", getAccountEquityChart);
 router.use("/v1/positions", getAccountOpenPositions);
 router.use("/v1/historicaltrades", getAccountHistoricalTrades);
+router.use("/v1/servers", searchServerName);
 
 module.exports = router;
